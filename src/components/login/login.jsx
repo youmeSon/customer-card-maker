e73fb597d63e2968348cba70a7ec509963b29e8b
Youme <rosydarling@naver.com>
2022-08-22 22:29:39 +0100

    add Email link authentication

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -6,9 +6,20 @@ import styles from './login.module.css';
 
 
 const Login = ({authService}) => {
-    const onLogin = () => {
-        authService.login()
+    const onLogin = (event) => {
+        authService.login(event.currentTarget.textContent)
         .then(console.log)
+    }
+
+    const onLoginEmail = () => {
+        const error = document.querySelector(`.${styles.error}`);
+        const emailAddress = document.getElementById('LoginEmail').value;
+        if(emailAddress.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)){
+            authService.emailLogin(emailAddress);
+        } else {
+            error.classList.remove(styles.hide);
+        }
+        
     }
     return (
         <section className={styles.login}>
@@ -18,6 +29,15 @@ const Login = ({authService}) => {
                         <ul className={styles.list}>
                             <li className={styles.item}>
                                 <button className={styles.button} onClick={onLogin}>Google</button>
+                            </li>  
+                        </ul>
+                        <span className={styles.or}>or</span>
+                        <ul className={styles.list}>
+                            <li className={styles.item}>
+                                <input type="email" placeholder="Please enter your email" className={styles.input} id="LoginEmail"/>
+                                <button className={styles.button} onClick={onLoginEmail}>Email</button>
+                                <span className={`${styles.error} ${styles.hide}`} >Email address is invalid.</span>
+                                <span className={`${styles.success} ${styles.hide}`} >Please check your email inbox.</span>
                             </li>  
                         </ul>
                 </section>
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,14 +1,37 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, sendSignInLinkToEmail } from "firebase/auth";
 import firebaseApp from './firebase'
 
 
 class AuthService {
-    login() {
-        const authProvider = new GoogleAuthProvider();
+    login(provider) {
+        let authProvider;
         const auth = getAuth();
+        if(provider == 'Google'){
+            authProvider = new GoogleAuthProvider();
+            return signInWithPopup(auth, authProvider);
+        } 
+        
 
-        return signInWithPopup(auth, authProvider);
+        
     }
+    emailLogin(emailAddress) {
+        const actionCodeSettings = {
+            url: 'http://localhost:3000/',
+            handleCodeInApp: true,
+            
+          };
+        const auth = getAuth();
+        sendSignInLinkToEmail(auth, emailAddress, actionCodeSettings)
+            .then(() => {
+                window.alert("Please check your email inbox.");
+                
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ...
+            });
+                }
 }
 
 export default AuthService;
