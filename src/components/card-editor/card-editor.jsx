c0e0549f9ae119b90ea818525d7dfaca0ede52bb
Youme <rosydarling@naver.com>
2022-09-02 09:13:51 +0100

    add card edit form components

--- src/components/card-editor/card-editor.jsx
+++ src/components/card-editor/card-editor.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import Button from '../button/button';
 import styles from './card-editor.module.css';
+import ImageFileInput from './../image-file-input/image-file-input';
 
-const CardEditor = ({card}) => (
-            <h1>{card.id}</h1>
-    );
+const CardEditor = ({card}) => {
+    const {name, message, theme, item, itemNumber, email, mobileNumber, fileName, fileURL} = card;
+    const onSubmit = () => {
+
+    }
+    
+    return (
+        <form className={styles.form}>
+            <input className={styles.input} type="text" name="name" value={name} />
+            <input className={styles.input} type="text" name="email" value={email} />
+            <select className={styles.dropdown} name="theme" value={theme}>
+                <option value="dark" >Dark</option>
+                <option value="light">Light</option>
+                <option value="colourful">Colourful</option>
+            </select>
+            <input className={styles.input} type="text" name="mobileNumber" value={mobileNumber} />
+            <input className={styles.input} type="text" name="item" value={item} />
+            <input className={styles.input} type="text" name="itemNumber" value={itemNumber} />
+            <textarea className={styles.message} name="message" value={message}></textarea>
+            <Button name="Delete" onClick={onSubmit}/>
+            <div className ={styles.fileInput}>
+                <ImageFileInput />
+            </div>
+        </form>
+    )
+}
 
 export default CardEditor;
---
+++ src/components/image-file-input/image-file-input.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './image-file-input.module.css';
+
+const ImageFileInput = (props) => (
+            <button>Image</button>
+    );
+
+export default ImageFileInput;
